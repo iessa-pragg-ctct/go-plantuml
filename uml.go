e36ec1429f3dce301409b9d146831d123471ce40
Iessa Pragg <iessa_pragg@trimble.com>
2024-02-08 14:57:05 +1300

    More specific logging

--- uml.go
+++ uml.go
@@ -13,6 +13,7 @@ func UML(src []byte, args ...string) (dist []byte, err error) {
 
 	path, err = exec.LookPath("plantuml")
 	if err != nil {
+		log.Println("could not find plantuml in path")
 		log.Println(err)
 		return
 	}
@@ -23,24 +24,28 @@ func UML(src []byte, args ...string) (dist []byte, err error) {
 
 	stdin, err = cmd.StdinPipe()
 	if err != nil {
+		log.Println("failed to set up stdin pipe")
 		log.Println(err)
 		return
 	}
 
 	_, err = stdin.Write(src)
 	if err != nil {
+		log.Println("failed to write to stdin")
 		log.Println(err)
 		return
 	}
 
 	stdin.Close()
 	if err != nil {
+		log.Println("failed to close stdin")
 		log.Println(err)
 		return
 	}
 
 	dist, err = cmd.CombinedOutput()
 	if err != nil {
+		log.Println("failed to run plantuml")
 		log.Println(err)
 		err = fmt.Errorf(string(dist))
 		dist = []byte{}
